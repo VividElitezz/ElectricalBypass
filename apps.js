a3f1854447fed1184da56617bc3a08e90c482b10
VividElitezz <aidenjdouglas09@gmail.com>
2025-03-13 09:20:33 -0400

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,55 @@
+function openApp(url) {
+  let encodedUrl = __uv$config.prefix + __uv$config.encodeUrl(url);
+  let iframe = document.getElementById("app-frame");
+  iframe.src = encodedUrl;
+
+  // Resize iframe dynamically when it loads
+  iframe.onload = function () {
+    resizeIframe();
+  };
+
+  // Request full-screen mode
+  if (iframe.requestFullscreen) {
+    iframe.requestFullscreen();
+  } else if (iframe.mozRequestFullScreen) {
+    iframe.mozRequestFullScreen();
+  } else if (iframe.webkitRequestFullscreen) {
+    iframe.webkitRequestFullscreen();
+  } else if (iframe.msRequestFullscreen) {
+    iframe.msRequestFullscreen();
+  }
+
+  // Show exit button when moving cursor to the top
+  document.addEventListener("mousemove", showExitButton);
+}
+
+function resizeIframe() {
+  var iframe = document.getElementById('app-frame');
+  var iframeDocument = iframe.contentWindow.document;
+  var bodyHeight = iframeDocument.body.scrollHeight;  // Get content height of the iframe
+  iframe.style.height = bodyHeight + "px";  // Set iframe height dynamically
+}
+
+function exitFullScreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+
+  // Hide exit button after exiting fullscreen
+  document.getElementById("exit-fullscreen").style.display = "none";
+  document.removeEventListener("mousemove", showExitButton);
+}
+
+function showExitButton(event) {
+  if (event.clientY < 50) { // Show exit button if cursor is at the top
+    document.getElementById("exit-fullscreen").style.display = "block";
+  } else {
+    document.getElementById("exit-fullscreen").style.display = "none";
+  }
+}
